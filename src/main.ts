0d3386dd62d7cb5d3052f3f552d0446a5aa91e59
Mark-André Hopf <mhopf@mark13.org>
2021-05-25 11:09:08 +0200

    refactor

--- src/main.ts
+++ src/main.ts
@@ -48,68 +48,16 @@ export function main() {
     document.body.appendChild(glframe)
 
     const gl = (canvas.getContext('webgl2') || canvas.getContext('experimental-webgl')) as WebGL2RenderingContext
-    if (!gl) {
+    if (gl === null) {
         alert('Unable to initialize WebGL. Your browser or machine may not support it.')
         return
     }
 
-    const vertextShaderProgram = `
-    attribute vec4 aVertexPosition;
-    attribute vec3 aVertexNormal;
-    attribute vec4 aVertexColor;
-
-    uniform mat4 uModelViewMatrix;
-    uniform mat4 uProjectionMatrix;
-    uniform mat4 uNormalMatrix;
-
-    varying lowp vec4 vColor;
-    varying highp vec3 vLighting;
-
-    void main(void) {
-        gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-        vColor = aVertexColor;
-
-        highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
-        highp vec3 ambientLight = vec3(0.1, 0.1, 0.1);
-
-        highp vec3 directionalLightColor1 = vec3(1, 0, 0);
-        highp vec3 directionalVector1 = normalize(vec3(0.85, 0.8, 0.75));
-        highp float directional1 = max(dot(transformedNormal.xyz, directionalVector1), 0.0);
-
-        highp vec3 directionalLightColor2 = vec3(0, 0, 1);
-        highp vec3 directionalVector2 = normalize(vec3(-0.85, 0.8, 0.75));
-        highp float directional2 = max(dot(transformedNormal.xyz, directionalVector2), 0.0);
-
-        vLighting = ambientLight + (directionalLightColor1 * directional1) + (directionalLightColor2 * directional2) ;
-    }`
-
-    // Fragment shader program
-
-    const fragmentShaderProgram = `
-    varying lowp vec4 vColor;
-    varying highp vec3 vLighting;
-    void main(void) {
-        gl_FragColor = vec4(vec3(1,1,1) * vLighting, 1.0);
-        // gl_FragColor = vec4(vColor.xyz * vLighting, 1.0);
-    }`
-    const shaderProgram = compileShaders(gl, vertextShaderProgram, fragmentShaderProgram)
-
-    const programInfo: ProgramInfo = {
-        program: shaderProgram,
-        attribLocations: {
-            vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
-            vertexNormal: gl.getAttribLocation(shaderProgram, 'aVertexNormal'),
-            vertexColor: gl.getAttribLocation(shaderProgram, 'aVertexColor'),
-        },
-        uniformLocations: {
-            projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix')!,
-            modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix')!,
-            normalMatrix: gl.getUniformLocation(shaderProgram, 'uNormalMatrix')!
-        }
-    }
-
     const scene = createScene()
     const buffers = createAllBuffers(gl, scene)
+    const vertexShader = compileShader(gl, gl.VERTEX_SHADER, vertextShaderProgram)
+    const fragmentShader = compileShader(gl, gl.FRAGMENT_SHADER, fragmentShaderProgram)
+    const programInfo = linkProgram(gl, vertexShader, fragmentShader)
 
     let t0 = 0
     function render(t1: number) {
@@ -197,6 +145,110 @@ function createScene(): Scene {
         }
     }
     return scene
+}
+
+function createAllBuffers(gl: WebGL2RenderingContext, scene: Scene): Buffers {
+    var colors: number[] = []
+    for (var j = 0; j < scene.faceColors.length; ++j) {
+        const c = scene.faceColors[j]
+        for(let i=0; i<4; ++i)
+            colors.push(...c)
+    }
+
+    return {
+        position: createBuffer(gl, gl.ARRAY_BUFFER, gl.STATIC_DRAW, Float32Array, scene.vertex),
+        normal: createBuffer(gl, gl.ARRAY_BUFFER, gl.STATIC_DRAW, Float32Array, scene.normals),
+        color: createBuffer(gl, gl.ARRAY_BUFFER, gl.STATIC_DRAW, Float32Array, colors),
+        indices: createBuffer(gl, gl.ELEMENT_ARRAY_BUFFER, gl.STATIC_DRAW, Uint16Array, scene.indices),
+    }
+}
+
+function createBuffer<T>(gl: WebGL2RenderingContext, target: GLenum, usage: GLenum, type: any, data: number[]): WebGLBuffer {
+    const buffer = gl.createBuffer()
+    if (buffer === null)
+        throw Error("Unable to create WebGLBuffer")
+    gl.bindBuffer(target, buffer)
+    gl.bufferData(target, new type(data), usage)
+    return buffer
+}
+
+function compileShader(gl: WebGL2RenderingContext, type: GLenum, source: string): WebGLShader {
+    const shader = gl.createShader(type)
+    if (shader === null)
+        throw Error("Unable to create WebGLShader")
+    gl.shaderSource(shader, source)
+    gl.compileShader(shader)
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        gl.deleteShader(shader)
+        throw Error(`An error occurred compiling the ${type} WebGLShader: ${gl.getShaderInfoLog(shader)}`)
+    }
+    return shader
+}
+
+const vertextShaderProgram = `
+attribute vec4 aVertexPosition;
+attribute vec3 aVertexNormal;
+attribute vec4 aVertexColor;
+
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+uniform mat4 uNormalMatrix;
+
+varying lowp vec4 vColor;
+varying highp vec3 vLighting;
+
+void main(void) {
+    gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    vColor = aVertexColor;
+
+    highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
+    highp vec3 ambientLight = vec3(0.1, 0.1, 0.1);
+
+    highp vec3 directionalLightColor1 = vec3(1, 0, 0);
+    highp vec3 directionalVector1 = normalize(vec3(0.85, 0.8, 0.75));
+    highp float directional1 = max(dot(transformedNormal.xyz, directionalVector1), 0.0);
+
+    highp vec3 directionalLightColor2 = vec3(0, 0, 1);
+    highp vec3 directionalVector2 = normalize(vec3(-0.85, 0.8, 0.75));
+    highp float directional2 = max(dot(transformedNormal.xyz, directionalVector2), 0.0);
+
+    vLighting = ambientLight + (directionalLightColor1 * directional1) + (directionalLightColor2 * directional2) ;
+}`
+
+const fragmentShaderProgram = `
+varying lowp vec4 vColor;
+varying highp vec3 vLighting;
+void main(void) {
+    gl_FragColor = vec4(vec3(1,1,1) * vLighting, 1.0);
+    // gl_FragColor = vec4(vColor.xyz * vLighting, 1.0);
+}`
+
+function linkProgram(gl: WebGL2RenderingContext, vertexShader: WebGLShader, fragmentShader: WebGLShader): ProgramInfo {
+    const program = gl.createProgram()
+    if (program === null) {
+        throw Error(`Unable to create WebGLProgram`)
+    }
+    gl.attachShader(program, vertexShader)
+    gl.attachShader(program, fragmentShader)
+    gl.linkProgram(program)
+
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        throw Error(`Unable to initialize WebGLProgram: ${gl.getProgramInfoLog(program)}`)
+    }
+
+    return {
+        program: program,
+        attribLocations: {
+            vertexPosition: gl.getAttribLocation(program, 'aVertexPosition'),
+            vertexNormal: gl.getAttribLocation(program, 'aVertexNormal'),
+            vertexColor: gl.getAttribLocation(program, 'aVertexColor'),
+        },
+        uniformLocations: {
+            projectionMatrix: gl.getUniformLocation(program, 'uProjectionMatrix')!,
+            modelViewMatrix: gl.getUniformLocation(program, 'uModelViewMatrix')!,
+            normalMatrix: gl.getUniformLocation(program, 'uNormalMatrix')!
+        }
+    }
 }
 
 function drawScene(gl: WebGL2RenderingContext, programInfo: ProgramInfo, buffers: Buffers, scene: Scene) {
@@ -296,61 +348,4 @@ function drawScene(gl: WebGL2RenderingContext, programInfo: ProgramInfo, buffers
         const offset = 0
         gl.drawElements(gl.TRIANGLES, vertexCount, type, offset)
     }
-}
-
-function compileShaders(gl: WebGL2RenderingContext, vertexSharderSrc: string, fragmentShaderSrc: string): WebGLProgram {
-    const vertexShader = compileShader(gl, gl.VERTEX_SHADER, vertexSharderSrc)
-    const fragmentShader = compileShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSrc)
-
-    const program = gl.createProgram()
-    if (program === null) {
-        throw Error(`Unable to create WebGLProgram`)
-    }
-    gl.attachShader(program, vertexShader)
-    gl.attachShader(program, fragmentShader)
-    gl.linkProgram(program)
-
-    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-        throw Error(`Unable to initialize WebGLProgram: ${gl.getProgramInfoLog(program)}`)
-    }
-
-    return program
-}
-
-function compileShader(gl: WebGL2RenderingContext, type: GLenum, source: string): WebGLShader {
-    const shader = gl.createShader(type)
-    if (shader === null)
-        throw Error("Unable to create WebGLShader")
-    gl.shaderSource(shader, source)
-    gl.compileShader(shader)
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        gl.deleteShader(shader)
-        throw Error(`An error occurred compiling the ${type} WebGLShader: ${gl.getShaderInfoLog(shader)}`)
-    }
-    return shader
-}
-
-function createAllBuffers(gl: WebGL2RenderingContext, scene: Scene): Buffers {
-    var colors: number[] = []
-    for (var j = 0; j < scene.faceColors.length; ++j) {
-        const c = scene.faceColors[j]
-        for(let i=0; i<4; ++i)
-            colors.push(...c)
-    }
-
-    return {
-        position: createBuffer(gl, gl.ARRAY_BUFFER, gl.STATIC_DRAW, Float32Array, scene.vertex),
-        normal: createBuffer(gl, gl.ARRAY_BUFFER, gl.STATIC_DRAW, Float32Array, scene.normals),
-        color: createBuffer(gl, gl.ARRAY_BUFFER, gl.STATIC_DRAW, Float32Array, colors),
-        indices: createBuffer(gl, gl.ELEMENT_ARRAY_BUFFER, gl.STATIC_DRAW, Uint16Array, scene.indices),
-    }
-}
-
-function createBuffer<T>(gl: WebGL2RenderingContext, target: GLenum, usage: GLenum, type: any, data: number[]): WebGLBuffer {
-    const buffer = gl.createBuffer()
-    if (buffer === null)
-        throw Error("Unable to create WebGLBuffer")
-    gl.bindBuffer(target, buffer)
-    gl.bufferData(target, new type(data), usage)
-    return buffer
 }
